14a4fc62862a89dc0c5a554d2ad8324eac2ac200
ismelloa <israel.mello@economia.ufjf.br>
2024-05-02 17:18:40 -0600

    Changed the display messagess

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello assdsaddsaaSand World!");
+        Console.WriteLine("Hello Israel World!!!!g");
     }
 }
