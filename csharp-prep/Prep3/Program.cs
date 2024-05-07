5eea3e359062196b0ce2a4e570e2f7d13bead296
ismelloa <israel.mello@economia.ufjf.br>
2024-05-06 22:00:15 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+        int guessNumber = 0;
+        
+        while (guessNumber != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            //or use like this
+            // string userInput1 = Console.ReadLine();
+            // guessNumber = int.Parse(userInput1);
+            
+         
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            else if (guessNumber < magicNumber)
+            {
+            Console.WriteLine("higher");  
+            }
+            
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+                   
     }
 }
